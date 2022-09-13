ada046ea8f0936759fe587ef5a7e95b3507931d6
Brad Voris <40213182+bvoris@users.noreply.github.com>
2022-09-13 17:11:19 -0500

    Update dossierbuilder.py

--- dossierbuilder.py
+++ dossierbuilder.py
@@ -1,45 +1,40 @@
-#Title Screen
+titlescreen = """
+..#######...######..####.##....##.########.########..########...#######..########.####.##.......########.########.
+.##.....##.##....##..##..###...##....##....##.....##.##.....##.##.....##.##........##..##.......##.......##.....##
+.##.....##.##........##..####..##....##....##.....##.##.....##.##.....##.##........##..##.......##.......##.....##
+.##.....##..######...##..##.##.##....##....########..########..##.....##.######....##..##.......######...########.
+.##.....##.......##..##..##..####....##....##........##...##...##.....##.##........##..##.......##.......##...##..
+.##.....##.##....##..##..##...###....##....##........##....##..##.....##.##........##..##.......##.......##....##.
+..#######...######..####.##....##....##....##........##.....##..#######..##.......####.########.########.##.....##
+BY: BRAD VORIS
+"""
+disclaimer ="""
+I am not responsible for how you use the information gathered from OSINTPROFILER. Use this tool responsibly.
+"""
+print(titlescreen)
+print()
+print(disclaimer)
+input()
+firstname = input("What is the target's first name? ")
+lastname = input("What is the target's last/surname? ")
+state =  input("What is the target's state? ")
+stateabbr = input("What is the target's state abbreviation? ")
+city = input("What is the target's city ")
+MergedName = firstname + '-' + lastname
+truepeople = str(f'https://www.truepeoplesearch.com/results?name={firstname}%20{lastname}&citystatezip={state}')
+usphonebook = str(f'https://www.usphonebook.com/{MergedName}')
+findpeoplefast = str(f'https://findpeoplefast.net/people/{MergedName}/{stateabbr}')
+publicdatausa = str(f'https://www.publicdatausa.com/{MergedName}')
+spokeo = str(f'https://www.spokeo.com/{MergedName}?loaded=1')
+peoplefinders = str(f'https://www.peoplefinders.com/people/{MergedName}/{stateabbr}?landing=people')
+whitepages = str(f'https://www.whitepages.com/name/{MergedName}/{stateabbr}?fs=1&searchedName={firstname}%20{lastname}&searchedLocation={state}')
+searchpeoplefree = str(f'https://www.searchpeoplefree.com/find/{MergedName}/{stateabbr}/{city}')
+datalist = [truepeople, usphonebook, findpeoplefast, publicdatausa, spokeo, peoplefinders, whitepages, searchpeoplefree]
+print()
+print("Compiled Search Links listed Below...")
+print()
+for url in datalist:
+    print(url)
+print("Complete!")
 
-#User Inputs
-firstname = input("What is the target's first name?")
-lastname = input("What is the target's last/surname?")
-alternativename = input("What is the target's alternative name? Example: First Name Michael Alternative Name Mike")
-state =  input("What is the target's state?")
-stateabbr =  input("What is the target's state abbreviation?")
-#middlename = input("what is the target's middle name?")
-#address = input("what is the target's address?")
-#city = input("What is the target's city?")
-#zipcode = input("What is the target's zipcode?")
-#accountusername = input("What is the target's account username?")
 
-#Search Arrays
-#MergedNameSearchCriteriaArray[0] = "https://clustrmaps.com/persons/firstname-lastname/state, https://www.usphonebook.com/firstname-lastname, https://findpeoplefast.net/people/firstname-lastname/stateabbr, https://www.publicdatausa.com/firstname-lastname"
-#AlternativeNameSearchCriteriaArray[0] = "https://clustrmaps.com/persons/alternativename-lastname/state, https://www.usphonebook.com/alternativename-lastname, https://findpeoplefast.net/people/alternativename-lastname/stateabbr, https://www.publicdatausa.com/alternativename-lastname"
-#AddressSearchCriteriaArray[0] = ""
-#CitySearchCriteriaArray[0] = ""
-#StateSearchCriteriaArray[0] = ""
-#StateAbbrSearchCriteriaArray[0] = ""
-#ZipCodeSearchCriteriaArray[0] = ""
-#AccountUsernameSearchCriteriaArray[0] = ""
-
-#Functions for URIs
-#from urllib.request import urlopen
-#url = "https://www.cyberforgesecurity.com"
-#page = urlopen(url)
-#html_bytes = page.read()
-#html = html_bytes.decode("utf-8")
-#print(html)
-#start_index = html.find("h-global-transition-all") + len("h-global-transition-all")
-#end_index = html.find("</div>")
-#data = html[start_index:end_index]
-#print(data)
-
-#BeatifulSoup Web Scrapper
-from bs4 import BeautifulSoup
-from urllib.request import urlopen
-
-url = "https://www.cyberforgesecurity.com"
-page = urlopen(url)
-html = page.read().decode("utf-8")
-soup = BeautifulSoup(html, "html.parser")
-print(soup.get_text())
