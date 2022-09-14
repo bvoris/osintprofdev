96d553d6a4e7663795806f28ec9029ef50ca8ba5
Brad Voris <40213182+bvoris@users.noreply.github.com>
2022-09-14 09:38:38 -0500

    Update osintprofiler.py

--- osintprofiler.py
+++ osintprofiler.py
@@ -3,7 +3,7 @@
 # Version: 0.05
 # Requirements: requests, types-requests, & BeautifulSoup4 
 # Description: This tool gathers information from the end user about a specific target. First name, last/surname, location, etc. are gathered to generate a list and
-# scrape specific websites to gather additional information about the target. This is a reconnaissance that can be used by red teams to help facilitate penetration testing through social engineering.
+# scrape specific websites to gather additional information about the target. This is a reconnaissance tool that can be used by red teams to help facilitate penetration testing through social engineering.
 #
 
 # Import Section
